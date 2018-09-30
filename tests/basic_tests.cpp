26ce47892fa67b3baa9dae4d89bcaeb41fd1c673
Fatih BAKIR <mfatihbakir@gmail.com>
2018-09-29 23:32:59 -0700

    couple more tests

--- tests/basic_tests.cpp
+++ tests/basic_tests.cpp
@@ -28,3 +28,58 @@ TEST_CASE("very basic", "[tipe]")
 
     REQUIRE(res == 4);
 }
+
+TEST_CASE("multi path", "[tipe]")
+{
+    std::vector<int> vals;
+
+    auto nodes = tip::make_nodes(
+            []{ return 1; },
+            [](auto x) { return x + x; },
+            [](auto x) { return x * x; },
+            [&](auto x) { vals.push_back(x); }
+    );
+
+    auto ids = tip::get_ids(nodes);
+    auto [base, add, mult, fin] = ids;
+
+    auto edges = tip::make_edges(ids)
+            .connect(base | add | mult | fin)
+            .connect(add | fin);
+
+    auto g = tip::make_graph(std::move(nodes), edges);
+
+    tip::execute(g, base);
+
+    REQUIRE(std::count(vals.begin(), vals.end(), 4) == 1);
+    REQUIRE(std::count(vals.begin(), vals.end(), 2) == 1);
+}
+
+TEST_CASE("init param", "[tipe]")
+{
+    std::vector<int> vals;
+
+    auto nodes = tip::make_nodes(
+            [](auto x){ return x; },
+            [](auto x) { return x + x; },
+            [](auto x) { return x * x; },
+            [&](auto x) { vals.push_back(x); }
+    );
+
+    auto ids = tip::get_ids(nodes);
+    auto [base, add, mult, fin] = ids;
+
+    auto edges = tip::make_edges(ids)
+            .connect(base | add | mult | fin)
+            .connect(add | fin);
+
+    auto g = tip::make_graph(std::move(nodes), edges);
+
+    tip::execute(g, base, 1);
+    tip::execute(g, base, 4);
+
+    REQUIRE(std::count(vals.begin(), vals.end(), 4) == 1);
+    REQUIRE(std::count(vals.begin(), vals.end(), 2) == 1);
+    REQUIRE(std::count(vals.begin(), vals.end(), 8) == 1);
+    REQUIRE(std::count(vals.begin(), vals.end(), 64) == 1);
+}
